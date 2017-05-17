5f00bfb6eeb5a7d7244bcaa539d77543270e443d
Kenneth fang <kwf37@cornell.edu>
2017-05-16 20:01:06 -0400

    Adding files given to me by Sanskriti

---
+++ rapsberrypi.py
@@ -0,0 +1,14 @@
+from gpiozero import MotionSensor
+from picamera import Picamera
+
+pir = MotionSensor(4)
+camera = PiCamera()
+
+while True:
+    pir.wait_for_motion()
+    filename = datetime.now().strftime("%Y-%m-%d_%H.%M.%S.h264")
+    camera.start_recording(filename)
+    pir.wait_for_no_motion()
+    camera.stop_recording()
+
+    
